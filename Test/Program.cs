1760fa236766df48a30dea9cc1c3e48467568665
ism4045 <ism4045@naver.com>
2022-03-28 18:44:24 +0900

    I'm First Branch

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Universe!");
+            Console.WriteLine("Hello I'm First Branch!");
         }
     }
 }
